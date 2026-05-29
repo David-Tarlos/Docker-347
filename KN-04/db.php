01e3571c5d42636f23a7ea75e883e6183d48f5c0
Leon Wulff <leon.wulff@edu.tbz.ch>
2026-05-29 09:15:01 +0200

    KN-04 done

---
+++ KN-04/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'm347-kn04a-db';
+$user = 'root';
+$pass = 'Test1234!';
+$db   = 'mysql';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die('Verbindung fehlgeschlagen: ' . $conn->connect_error);
+}
+
+echo 'Verbindung zur Datenbank erfolgreich auf Host ' . $host;
+
+$conn->close();
